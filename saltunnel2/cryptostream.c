1c8abdb15788ca47af591471f19eceebf6353019
Jay Sullivan <jaythomassullivan@gmail.com>
2019-12-10 00:02:22 -0500

    oops that shouldn't have been a loop

--- saltunnel2/cryptostream.c
+++ saltunnel2/cryptostream.c
@@ -14,13 +14,13 @@
 int cryptostream_identity_feed(cryptostream* cs, unsigned char* k) {
     char buf[512];
     ssize_t n;
-    for(;;) {
-        try((n = uninterruptable_read(read, cs->from_fd, buf, sizeof(buf)))) || oops_fatal("failed to read");
-        if(n==0)
-            break;
-        try(uninterruptable_write(write, cs->to_fd, buf, (unsigned int)(n))) || oops_fatal("failed to write");
-        fprintf(stderr,"cryptostream: fed %d bytes\n",(int)n);
-    }
+
+    try((n = uninterruptable_read(read, cs->from_fd, buf, sizeof(buf)))) || oops_fatal("failed to read");
+    if(n==0)
+        return 0;
+    try(uninterruptable_write(write, cs->to_fd, buf, (unsigned int)(n))) || oops_fatal("failed to write");
+    fprintf(stderr,"cryptostream: fed %d bytes\n",(int)n);
+    
     return 0;
 }
 
--- saltunnel2/test.c
+++ saltunnel2/test.c
@@ -115,6 +115,44 @@ void test3() {
     
 }
 
+// test4: saltunnel works with cryptostream
+void test4() {
+    
+    // Create four "fake files" (i.e., pipes)
+    int pipe_local_read[2];  try(pipe(pipe_local_read))  || oops_fatal("failed to create pipe");
+    int pipe_local_write[2]; try(pipe(pipe_local_write)) || oops_fatal("failed to create pipe");
+    int pipe_net_read[2];    try(pipe(pipe_net_read))    || oops_fatal("failed to create pipe");
+    int pipe_net_write[2];   try(pipe(pipe_net_write))   || oops_fatal("failed to create pipe");
+    
+    // Start with "expected value" available in both "readable" pipes
+    const char local_teststr_expected[] = "send_nt_pipe";
+    const char net_teststr_expected[] = "send_lc_pipe";
+    uninterruptable_write(write, pipe_local_read[1], local_teststr_expected, 12); close(pipe_local_read[1]);
+    uninterruptable_write(write, pipe_net_read[1],   net_teststr_expected,   12); close(pipe_net_read[1]);
+    
+    // Run saltunnel
+    cryptostream ingress = {
+        .op = cryptostream_identity_feed,
+        .from_fd = pipe_net_read[0],
+        .to_fd = pipe_local_write[1]
+    };
+    cryptostream egress = {
+        .op = cryptostream_identity_feed,
+        .from_fd = pipe_local_read[0],
+        .to_fd = pipe_net_write[1]
+    };
+    saltunnel(&ingress, &egress);
+    
+    // Read "actual value" from both "write" pipes
+    char local_teststr_actual[12+1] = {0};
+    char net_teststr_actual[12+1] = {0};
+    uninterruptable_read(read, pipe_local_write[0], local_teststr_actual, 12);
+    uninterruptable_read(read, pipe_net_write[0], net_teststr_actual, 12);
+    
+    // Assert "expected value" equals "actual value"
+    strcmp(local_teststr_expected, net_teststr_actual) == 0 || oops_fatal("local teststr did not match");
+    strcmp(net_teststr_expected, local_teststr_actual) == 0 || oops_fatal("net teststr did not match");
+}
 
 
 static void run(void (*the_test)(void), const char *test_name) {
@@ -127,6 +165,7 @@ int test() {
     run(test1, "test1");
     run(test2, "test2");
     run(test3, "test3");
+    run(test4, "test4");
     fprintf(stderr, "test: all tests passed\n");
     return 0;
 }
