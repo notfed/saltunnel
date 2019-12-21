5eb9937e47329723ea4e52c1ab0d8a4846ee0615
Jay Sullivan <jaythomassullivan@gmail.com>
2019-12-21 11:33:07 -0500

    cool logging

--- saltunnel2/log.c
+++ saltunnel2/log.c
@@ -6,15 +6,18 @@
 //  Copyright © 2019 Jay Sullivan. All rights reserved.
 //
 
+#include "log.h"
 #include <stdio.h>
+#include <string.h>
 
-void log_filename_idempotent_fill(char* log_filename, char* log_filename_from_macro, int len, int* log_filename_filled) {
-    if(!*log_filename_filled) {
-        strcpy(log_filename,log_filename_from_macro);
-        char* dot = strchr(log_filename, '.');
+char* log_filename_idempotent_fill(char* log_name, char* log_filename_from_macro, int len, int* log_name_filled) {
+    if(!*log_name_filled) {
+        strcpy(log_name,log_filename_from_macro);
+        char* dot = strchr(log_name, '.');
         if(dot!=0) {
             *dot=0;
         }
-        *log_filename_filled = 1;
+        *log_name_filled = 1;
     }
+    return log_name;
 }
--- saltunnel2/log.h
+++ saltunnel2/log.h
@@ -7,31 +7,28 @@
 #define log_h
 
 #include <stdio.h>
-#include <string.h>
-#include <unistd.h>
 
-void log_filename_idempotent_fill(char* log_filename, char* log_filename_from_macro, int len, int* log_filename_filled);
+char* log_filename_idempotent_fill(char* log_filename, char* log_filename_from_macro, int len, int* log_filename_filled);
 
-#define STATIC_ASSERT(COND,MSG) static void STATIC_ASSERT_MSG_FUNC() { int static_assertion_##MSG[(COND)?1:-1]; }
-STATIC_ASSERT(sizeof(__FILE_NAME__)<255,FILENAME_TOO_LONG);
+#define STATIC_ASSERT(COND,MSG) typedef char static_assertion_##MSG[(COND)?1:-1]
+STATIC_ASSERT(sizeof(__FILE_NAME__)<256,FILENAME_TOO_LONG);
 
 static char log_filename[256];
 static int log_filename_filled = 0;
 
-#define LOG_WRAP(cmd) ( log_filename_idempotent_fill(log_filename, __FILE_NAME__, sizeof(__FILE_NAME__), &log_filename_filled), cmd )
-#define LOG_FN(log_filename) log_filename
+#define LOG_NAME(log_filename) (log_filename_filled ? log_filename : log_filename_idempotent_fill(log_filename, __FILE_NAME__, sizeof(__FILE_NAME__), &log_filename_filled))
  
 #define LOG_0(level)                 !error
-#define LOG_1(level,x1)              LOG_WRAP(fprintf(stderr, "saltunnel: " level ": %s:%d: %s\n", LOG_FN(log_filename), __LINE__, x1))
-#define LOG_2(level,x1,x2)           LOG_WRAP(fprintf(stderr, "saltunnel: " level ": %s:%d: " x1 "\n", LOG_FN(log_filename), __LINE__, x2))
-#define LOG_3(level,x1,x2,x3)        LOG_WRAP(fprintf(stderr, "saltunnel: " level ": %s:%d: " x1 "\n", LOG_FN(log_filename), __LINE__, x2, x3))
-#define LOG_4(level,x1,x2,x3,x4)     LOG_WRAP(fprintf(stderr, "saltunnel: " level ": %s:%d: " x1 "\n", LOG_FN(log_filename), __LINE__, x2, x3, x4))
-#define LOG_5(level,x1,x2,x3,x4,x5)  LOG_WRAP(fprintf(stderr, "saltunnel: " level ": %s:%d: " x1 "\n", LOG_FN(log_filename), __LINE__, x2, x3, x4, x5))
-#define LOG_6(level,x1,x2,x3,x4,x5)  LOG_WRAP(fprintf(stderr, "saltunnel: " level ": %s:%d: " x1 "\n", LOG_FN(log_filename), __LINE__, x2, x3, x4, x5, x6))
-#define LOG_7(level,x1,x2,x3,x4,x5)  LOG_WRAP(fprintf(stderr, "saltunnel: " level ": %s:%d: " x1 "\n", LOG_FN(log_filename), __LINE__, x2, x3, x4, x5, x6, x7))
-#define LOG_8(level,x1,x2,x3,x4,x5)  LOG_WRAP(fprintf(stderr, "saltunnel: " level ": %s:%d: " x1 "\n", LOG_FN(log_filename), __LINE__, x2, x3, x4, x5, x6, x7, x8))
-#define LOG_9(level,x1,x2,x3,x4,x5)  LOG_WRAP(fprintf(stderr, "saltunnel: " level ": %s:%d: " x1 "\n", LOG_FN(log_filename), __LINE__, x2, x3, x4, x5, x6, x7, x8, x9))
-#define LOG_10(level,x1,x2,x3,x4,x5) LOG_WRAP(fprintf(stderr, "saltunnel: " level ": %s:%d: " x1 "\n", LOG_FN(log_filename), __LINE__, x2, x3, x4, x5, x6, x7, x8, x10))
+#define LOG_1(level,x1)              fprintf(stderr, "saltunnel: " level ": %s:%d: %s\n",     LOG_NAME(log_filename), __LINE__, x1)
+#define LOG_2(level,x1,x2)           fprintf(stderr, "saltunnel: " level ": %s:%d: " x1 "\n", LOG_NAME(log_filename), __LINE__, x2)
+#define LOG_3(level,x1,x2,x3)        fprintf(stderr, "saltunnel: " level ": %s:%d: " x1 "\n", LOG_NAME(log_filename), __LINE__, x2, x3)
+#define LOG_4(level,x1,x2,x3,x4)     fprintf(stderr, "saltunnel: " level ": %s:%d: " x1 "\n", LOG_NAME(log_filename), __LINE__, x2, x3, x4)
+#define LOG_5(level,x1,x2,x3,x4,x5)  fprintf(stderr, "saltunnel: " level ": %s:%d: " x1 "\n", LOG_NAME(log_filename), __LINE__, x2, x3, x4, x5)
+#define LOG_6(level,x1,x2,x3,x4,x5)  fprintf(stderr, "saltunnel: " level ": %s:%d: " x1 "\n", LOG_NAME(log_filename), __LINE__, x2, x3, x4, x5, x6)
+#define LOG_7(level,x1,x2,x3,x4,x5)  fprintf(stderr, "saltunnel: " level ": %s:%d: " x1 "\n", LOG_NAME(log_filename), __LINE__, x2, x3, x4, x5, x6, x7)
+#define LOG_8(level,x1,x2,x3,x4,x5)  fprintf(stderr, "saltunnel: " level ": %s:%d: " x1 "\n", LOG_NAME(log_filename), __LINE__, x2, x3, x4, x5, x6, x7, x8)
+#define LOG_9(level,x1,x2,x3,x4,x5)  fprintf(stderr, "saltunnel: " level ": %s:%d: " x1 "\n", LOG_NAME(log_filename), __LINE__, x2, x3, x4, x5, x6, x7, x8, x9)
+#define LOG_10(level,x1,x2,x3,x4,x5) fprintf(stderr, "saltunnel: " level ": %s:%d: " x1 "\n", LOG_NAME(log_filename), __LINE__, x2, x3, x4, x5, x6, x7, x8, x10)
  
 #define FUNC_CHOOSER(_f1, _f2, _f3, _f4, _f5, _f6, _f7, _f8, _f9, _f10, _f11, ...) _f11
 #define FUNC_RECOMPOSER(argsWithParentheses) FUNC_CHOOSER argsWithParentheses
