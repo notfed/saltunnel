6f782f9ef3b3a7a3c60589e2e68b3ea156273e81
Jay Sullivan <jaythomassullivan@gmail.com>
2020-02-03 22:52:23 -0500

    oddly, saltunnel_parallel doesn't improve perf

--- saltunnel/saltunnel_exchange_messages_parallel.c
+++ saltunnel/saltunnel_exchange_messages_parallel.c
@@ -22,65 +22,101 @@ static void fd_nonblock(int fd) {
     try(fcntl(fd, F_SETFL, flags|O_NONBLOCK)) || oops_fatal("setting O_NONBLOCK");
 }
 
-void exchange_messages_parallel(cryptostream *ingress, cryptostream *egress, unsigned char* key) {
-    
+typedef struct {
+    cryptostream *ingress; cryptostream *egress; unsigned char* key;
+} exchange_messages_thread_context;
+
+void* exchange_messages_egress(void* ctx_void) {
+    exchange_messages_thread_context* ctx = (exchange_messages_thread_context*)ctx_void;
+    cryptostream* egress = ctx->egress;
+    unsigned char* key = ctx->key;
+
     // Configure poll (we will poll both "readable" fds)
     struct pollfd pfds[] = {
-        { .fd = ingress->from_fd, .events = POLLIN|POLLHUP },
-        { .fd = ingress->to_fd,   .events = POLLOUT        },
         { .fd = egress->from_fd,  .events = POLLIN|POLLHUP },
         { .fd = egress->to_fd,    .events = POLLOUT        },
     };
     
     // Defensive Programming
-    fd_nonblock(ingress->from_fd); fd_nonblock(ingress->to_fd);
     fd_nonblock(egress->from_fd);  fd_nonblock(egress->to_fd);
     
     // Main Loop
-    while(pfds[0].fd != FD_EOF || pfds[1].fd != FD_EOF || pfds[2].fd != FD_EOF || pfds[3].fd != FD_EOF) {
+    while(pfds[0].fd != FD_EOF || pfds[1].fd != FD_EOF) {
         
         /* Poll */
-        log_debug("poll: polling [%2d->D->%2d, %2d->E->%2d]...", pfds[0].fd, pfds[1].fd,pfds[2].fd, pfds[3].fd);
-        try(poll(pfds,4,-1)) || oops_fatal("poll: failed to poll");
-        log_debug("poll: polled  [%2d->D->%2d, %2d->E->%2d].", pfds[0].fd, pfds[1].fd,pfds[2].fd, pfds[3].fd);
+        log_debug("poll: polling [%2d->D->%2d]...", pfds[0].fd, pfds[1].fd);
+        try(poll(pfds,2,-1)) || oops_fatal("poll: failed to poll");
+        log_debug("poll: polled  [%2d->D->%2d].", pfds[0].fd, pfds[1].fd);
         
         /* If an fd is ready, mark it as FD_READY */
         
         /* Loud Version*/
 //        if ((pfds[0].fd>=0) && (pfds[0].revents & (POLLIN|POLLHUP))) { log_debug("%d is ready to read from", pfds[0].fd); pfds[0].fd = FD_READY; }
 //        if ((pfds[1].fd>=0) && (pfds[1].revents & (POLLOUT)))        { log_debug("%d is ready to write to",  pfds[1].fd); pfds[1].fd = FD_READY; }
-//        if ((pfds[2].fd>=0) && (pfds[2].revents & (POLLIN|POLLHUP))) { log_debug("%d is ready to read from", pfds[2].fd); pfds[2].fd = FD_READY; }
-//        if ((pfds[3].fd>=0) && (pfds[3].revents & (POLLOUT)))        { log_debug("%d is ready to write to",  pfds[3].fd); pfds[3].fd = FD_READY; }
 //
         /* Quiet Version */
         if ((pfds[0].fd>=0) && (pfds[0].revents & (POLLIN|POLLHUP))) { pfds[0].fd = FD_READY; }
         if ((pfds[1].fd>=0) && (pfds[1].revents & (POLLOUT)))        { pfds[1].fd = FD_READY; }
-        if ((pfds[2].fd>=0) && (pfds[2].revents & (POLLIN|POLLHUP))) { pfds[2].fd = FD_READY; }
-        if ((pfds[3].fd>=0) && (pfds[3].revents & (POLLOUT)))        { pfds[3].fd = FD_READY; }
 //
         //
         // Handle egress data
         //
 
         // read from 'from' when: 'from' is ready, and buffers not full
-        if ((pfds[2].fd == FD_READY) && cryptostream_encrypt_feed_canread(egress)) {
+        if ((pfds[0].fd == FD_READY) && cryptostream_encrypt_feed_canread(egress)) {
             int r = cryptostream_encrypt_feed_read(egress,key);
-            if(r>0) { pfds[2].fd = egress->from_fd; }
-            if(r==0) { pfds[2].fd = FD_EOF; }
+            if(r>0) { pfds[0].fd = egress->from_fd; }
+            if(r==0) { pfds[0].fd = FD_EOF; }
         }
         
         // write to 'to' when: 'to' is ready, and buffers not empty
-        if ((pfds[3].fd == FD_READY) && cryptostream_encrypt_feed_canwrite(egress)) {
+        if ((pfds[1].fd == FD_READY) && cryptostream_encrypt_feed_canwrite(egress)) {
             cryptostream_encrypt_feed_write(egress,key);
-            pfds[3].fd = egress->to_fd;
+            pfds[1].fd = egress->to_fd;
         }
         
         // close 'to' when: 'from' is EOF, and all buffers are empty
-        if(pfds[2].fd == FD_EOF && pfds[3].fd != FD_EOF && !cryptostream_encrypt_feed_canwrite(egress)) {
+        if(pfds[0].fd == FD_EOF && pfds[1].fd != FD_EOF && !cryptostream_encrypt_feed_canwrite(egress)) {
             log_debug("egress is done; closing egress->to_fd (%d)", egress->to_fd);
             try(close(egress->to_fd)) || oops_fatal("failed to close");
-            pfds[3].fd = FD_EOF;
+            pfds[1].fd = FD_EOF;
         }
+
+    }
+    log_debug("all fds are closed [%d,%d,%d,%d]; done polling", ingress->from_fd, ingress->to_fd, egress->from_fd, egress->to_fd);
+    return 0;
+}
+void* exchange_messages_ingress(void* ctx_void) {
+    exchange_messages_thread_context* ctx = (exchange_messages_thread_context*)ctx_void;
+    cryptostream* ingress = ctx->ingress;
+    unsigned char* key = ctx->key;
+    
+    // Configure poll (we will poll both "readable" fds)
+    struct pollfd pfds[] = {
+        { .fd = ingress->from_fd, .events = POLLIN|POLLHUP },
+        { .fd = ingress->to_fd,   .events = POLLOUT        },
+    };
+    
+    // Defensive Programming
+    fd_nonblock(ingress->from_fd); fd_nonblock(ingress->to_fd);
+    
+    // Main Loop
+    while(pfds[0].fd != FD_EOF || pfds[1].fd != FD_EOF) {
+        
+        /* Poll */
+        log_debug("poll: polling [%2d->D->%2d]...", pfds[0].fd, pfds[1].fd);
+        try(poll(pfds,2,-1)) || oops_fatal("poll: failed to poll");
+        log_debug("poll: polled  [%2d->D->%2d].", pfds[0].fd, pfds[1].fd);
+        
+        /* If an fd is ready, mark it as FD_READY */
+        
+        /* Loud Version*/
+//        if ((pfds[0].fd>=0) && (pfds[0].revents & (POLLIN|POLLHUP))) { log_debug("%d is ready to read from", pfds[0].fd); pfds[0].fd = FD_READY; }
+//        if ((pfds[1].fd>=0) && (pfds[1].revents & (POLLOUT)))        { log_debug("%d is ready to write to",  pfds[1].fd); pfds[1].fd = FD_READY; }
+//
+        /* Quiet Version */
+        if ((pfds[0].fd>=0) && (pfds[0].revents & (POLLIN|POLLHUP))) { pfds[0].fd = FD_READY; }
+        if ((pfds[1].fd>=0) && (pfds[1].revents & (POLLOUT)))        { pfds[1].fd = FD_READY; }
 
         //
         // Handle ingress data
@@ -108,4 +144,29 @@ void exchange_messages_parallel(cryptostream *ingress, cryptostream *egress, uns
 
     }
     log_debug("all fds are closed [%d,%d,%d,%d]; done polling", ingress->from_fd, ingress->to_fd, egress->from_fd, egress->to_fd);
+    return 0;
+}
+
+void exchange_messages_parallel(cryptostream *ingress, cryptostream *egress, unsigned char* key) {
+    
+    exchange_messages_thread_context ctx;
+    ctx.ingress = ingress;
+    ctx.egress = egress;
+    ctx.key = key;
+
+    pthread_t egress_thread;
+    pthread_t ingress_thread;
+    
+    pthread_create(&egress_thread, NULL, exchange_messages_egress, (void*)&ctx)==0
+    || oops_fatal("pthread_create failed");
+    
+    pthread_create(&ingress_thread, NULL, exchange_messages_ingress, (void*)&ctx)==0
+    || oops_fatal("pthread_create failed");
+    
+    pthread_join(egress_thread, NULL)==0
+    || oops_fatal("pthread_create failed");
+    
+    pthread_join(ingress_thread, NULL)==0
+    || oops_fatal("pthread_create failed");
+    
 }
