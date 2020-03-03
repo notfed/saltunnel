913cc50db7300cc63fd49473811527363cb8c5ec
Jay Sullivan <jaythomassullivan@gmail.com>
2020-03-02 23:53:02 -0500

    load key

--- saltunnel/saltunnel_client.c
+++ saltunnel/saltunnel_client.c
@@ -3,10 +3,25 @@
 //  saltunnel
 //
 
-#include <stdio.h>
 #include "sodium.h"
 #include "oops.h"
 #include "saltunnel_tcp_client_forwarder.h"
+#include "uninterruptable.h"
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+static int open_readn_close(const char* file_path, unsigned char* out_buf, int len) {
+    int fd = open(file_path, O_RDONLY);
+    if(fd<0)
+        oops_warn("failed to open fd");
+    int r = (int)uninterruptable_readn(fd, (char*)out_buf, len);
+    if(close(fd)<0)
+        oops_warn("failed to close fd");
+    return r;
+}
 
 void error_usage() {
     fprintf(stderr,"saltunnel-server: %s: usage: saltunnel-server <key-file> <from-host>:<from-port> <to-host>:<to-port>\n");
@@ -24,7 +39,9 @@ int main(int argc, char * argv[])
     
     // Read key
     const char* key_path = argv[1];
-    unsigned char key[32] = {0}; // TODO
+    unsigned char key[32];
+    if(open_readn_close(key_path,key,32)<0)
+        oops_fatal("failed to read key");
     
     // Determine arguments
     char* from_colon = strchr(argv[2], ':');
