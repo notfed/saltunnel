42092a54fee4a60e432c28e58544458eeb20ae48
Jay Sullivan <jaythomassullivan@gmail.com>
2020-03-11 20:08:30 -0400

    consolidate uints

--- saltunnel/hypercounter.h
+++ saltunnel/hypercounter.h
@@ -7,8 +7,8 @@
 #define hypercounter_h
 
 // Gives two values:
-// (1) a unique value uniquely identifying this {machine,last_reboot_time}, and
-// (2) a unique, monotonically increasing number since the last reboot
+// (1) a 16-byte value uniquely identifying this {machine,last_reboot_time}, and
+// (2) an 8-byte unique, monotonically increasing number since the last reboot
 int hypercounter(unsigned char machine_id_out[16],     // H(MAC address + epoch-second time)
                  unsigned char monotonic_time_out[8]); // monotonically-increasing nanoseconds-since-reboot
 
--- saltunnel/uint32.c
+++ saltunnel/uint32.c
@@ -1,5 +1,25 @@
 #include "uint32.h"
 
+void uint32_pack(char s[4],uint32_t u)
+{
+    s[0] = u & 255;
+    u >>= 8;
+    s[1] = u & 255;
+    u >>= 8;
+    s[2] = u & 255;
+    s[3] = u >> 8;
+}
+
+void uint32_pack_big(char s[4],uint32_t u)
+{
+    s[3] = u & 255;
+    u >>= 8;
+    s[2] = u & 255;
+    u >>= 8;
+    s[1] = u & 255;
+    s[0] = u >> 8;
+}
+
 void uint32_unpack(char s[4],uint32_t *u)
 {
   uint32_t result;
