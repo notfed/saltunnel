f706269f07f4664ac6e82c98097d6e8f6781ed08
Jay Sullivan <jaythomassullivan@gmail.com>
2020-03-18 22:00:04 -0400

    .

--- saltunnel/hypercounter.h
+++ saltunnel/hypercounter.h
@@ -9,7 +9,7 @@
 // Gives two values:
 // (1) a 16-byte value uniquely identifying this {machine,last_reboot_time}, and
 // (2) an 8-byte unique, monotonically increasing number since the last reboot
-int hypercounter(unsigned char machine_id_out[16],     // H(MAC address + epoch-second time)
+int hypercounter(unsigned char machine_id_out[16],     // H(MAC address + last-reboot time)
                  unsigned char monotonic_time_out[8]); // monotonically-increasing nanoseconds-since-reboot
 
 #endif /* hypercounter */
