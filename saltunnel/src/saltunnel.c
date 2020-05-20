42ff7b3be120414158d3cf897ce9656d551b9455
jay <jay@identity.pub>
2020-05-19 21:14:25 -0400

    remove unused comment

--- saltunnel/src/saltunnel.c
+++ saltunnel/src/saltunnel.c
@@ -15,17 +15,6 @@
 #include <fcntl.h>
 
 void saltunnel(cryptostream* ingress, cryptostream* egress) {
-
-//    // Input Long-term Key (For now, just hard-coding to [0..31])
-//    unsigned char long_term_key[32] = {0};
-//    for(int i = 0; i<32;  i++)
-//        long_term_key[i] = i;
-//
-//    // Key Exchange
-//    unsigned char session_key[32] = {0};
-//    exchange_session_key(ingress, egress, long_term_key, session_key);
-    
-    // Message Exchange
     if(SALTUNNEL_PUMP_THREADS==1)
         exchange_messages_serial(ingress, egress);
     else if(SALTUNNEL_PUMP_THREADS==2)
