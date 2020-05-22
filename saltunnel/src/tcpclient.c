1569f607cde582417754d2c4e007e506506d5a4a
Jay Sullivan <jaythomassullivan@gmail.com>
2020-05-22 12:51:58 -0400

    connect_with_timestamp should work w/ SIGSTOP/SIGCONT

--- saltunnel/src/tcpclient.c
+++ saltunnel/src/tcpclient.c
@@ -16,40 +16,57 @@
 #include <fcntl.h>
 #include <stdlib.h>
 #include <poll.h>
+#include <time.h>
 
-int fd_block(int sockfd) { return fcntl(sockfd,F_SETFL,fcntl(sockfd,F_GETFL,0) & ~O_NONBLOCK); }
-int fd_unblock(int sockfd) { return fcntl(sockfd,F_SETFL,fcntl(sockfd,F_GETFL,0) | O_NONBLOCK); }
-
-static int connect_with_timeout(int sockfd, const struct sockaddr *addr, socklen_t addrlen, unsigned int timeout) {
+static int connect_with_timeout(int sockfd, const struct sockaddr *addr, socklen_t addrlen, unsigned int timeout_ms) {
     int rc = 0;
     // Set O_NONBLOCK
     int sockfd_flags_before;
     if((sockfd_flags_before=fcntl(sockfd,F_GETFL,0)<0)) return -1;
     if(fcntl(sockfd,F_SETFL,sockfd_flags_before | O_NONBLOCK)<0) return -1;
-    // Start connecting
-    if (connect(sockfd, addr, addrlen)<0) {
-        // Did connect return an error? If so, fail.
-        if ((errno != EWOULDBLOCK) && (errno != EINPROGRESS)) {
-            rc = -1;
-        }
-        // Did connect begin connecting?
-        else {
-            // Wait for connect to complete
-            struct pollfd pfds[] = { { .fd = sockfd, .events = POLLOUT } };
-            rc = poll(pfds, 1, timeout);
-            // Did poll timeout? If so, fail.
-            if(rc==0) {
-                close(sockfd);
-                errno = ETIMEDOUT;
-                return -1;
+    // Start connecting (asynchronously)
+    do {
+        if (connect(sockfd, addr, addrlen)<0) {
+            // Did connect return an error? If so, we'll fail.
+            if ((errno != EWOULDBLOCK) && (errno != EINPROGRESS)) {
+                rc = -1;
+            }
+            // Otherwise, we'll wait for it to complete.
+            else {
+                // Set a deadline timestamp 'timeout' ms from now (needed b/c poll can be interrupted)
+                struct timespec now;
+                if(clock_gettime(CLOCK_MONOTONIC, &now)<0) { rc=-1; break; }
+                struct timespec deadline = { .tv_sec = now.tv_sec,
+                                             .tv_nsec = now.tv_nsec + timeout_ms*1000000l};
+                // Wait for the connection to complete.
+                do {
+                    // Calculate how long until the deadline
+                    if(clock_gettime(CLOCK_MONOTONIC, &now)<0) { rc=-1; break; }
+                    int ms_until_deadline = (int)(  (deadline.tv_sec  - now.tv_sec)*1000l
+                                                  + (deadline.tv_nsec - now.tv_nsec)/1000000l);
+                    if(ms_until_deadline<0) { rc=0; break; }
+                    // Wait for connect to complete (or for the timeout deadline)
+                    struct pollfd pfds[] = { { .fd = sockfd, .events = POLLOUT } };
+                    rc = poll(pfds, 1, ms_until_deadline);
+                }
+                // If poll was interrupted, try again.
+                while(rc==-1 && errno==EINTR);
+                // Did poll timeout? If so, fail.
+                if(rc==0) {
+                    errno = ETIMEDOUT;
+                    rc=-1;
+                }
             }
         }
-    }
+    } while(0);
     // Restore original O_NONBLOCK state
     if(fcntl(sockfd,F_SETFL,sockfd_flags_before)<0) return -1;
     // Success
     return rc;
 }
+
+int fd_block(int sockfd) { return fcntl(sockfd,F_SETFL,fcntl(sockfd,F_GETFL,0) & ~O_NONBLOCK); }
+int fd_unblock(int sockfd) { return fcntl(sockfd,F_SETFL,fcntl(sockfd,F_GETFL,0) | O_NONBLOCK); }
 
 static int cleanup_then_oops_sys(int socket, const char* warning) {
     int e = errno;
