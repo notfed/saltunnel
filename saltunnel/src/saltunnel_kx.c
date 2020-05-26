20c7609a25272f06b113c565a26e0ab4ed721b1e
Jay Sullivan <jaythomassullivan@gmail.com>
2020-05-25 21:18:00 -0400

    simplify proof algorithm (easier to analyze)

--- saltunnel/src/saltunnel_kx.c
+++ saltunnel/src/saltunnel_kx.c
@@ -183,9 +183,7 @@ int saltunnel_kx_serverhi_trywrite(serverhi* serverhi_plaintext_pinned,
     
     // Generate proof that we know both session keys
     for(int i = 0; i<16; i++) {
-        serverhi_plaintext_pinned->proof[i] =
-          session_shared_keys_pinned[0+i]  ^ session_shared_keys_pinned[16+i]
-        ^ session_shared_keys_pinned[32+i] ^ session_shared_keys_pinned[48+i];
+        serverhi_plaintext_pinned->proof[i] = session_shared_keys_pinned[0+i] ^ session_shared_keys_pinned[48+i];
     }
     
     // Encrypt clienthi
@@ -254,12 +252,10 @@ int saltunnel_kx_serverhi_tryread(serverhi* serverhi_plaintext_pinned,
     // Verify proof
     unsigned char expected_proof[16];
     for(int i = 0; i<16; i++) {
-        expected_proof[i] =
-            session_shared_keys_pinned[0+i]  ^ session_shared_keys_pinned[16+i]
-          ^ session_shared_keys_pinned[32+i] ^ session_shared_keys_pinned[48+i];
+        expected_proof[i] = session_shared_keys_pinned[0+i]  ^ session_shared_keys_pinned[48+i];
     }
     if(sodium_compare(expected_proof, serverhi_plaintext_pinned->proof, 16)!=0)
-        return oops_error("authentication failed: proof was invalid");
+        return oops("authentication failed: proof was invalid");
     
     // Copy their_pk to output
     memcpy(their_pk_out_pinned, serverhi_plaintext_pinned->public_key, 32);
